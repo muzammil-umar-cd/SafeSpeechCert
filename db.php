2427020ec15a2837dc37639f266a64214aea8886
Muhammad Muzammil Umar <muzammil.coredigitals@gmail.com>
2025-03-19 01:30:58 +0500

    code-fix

--- db.php
+++ db.php
@@ -1,5 +1,7 @@
 <?php
 
+// $conn = new mysqli("localhost", "root", "", "safe_speech_cert");
+
 $conn = new mysqli("localhost", "demoprojectonlin_scc_user", "v5soa@POji4m", "demoprojectonlin_scc");
 
 if ($conn->connect_error) {
