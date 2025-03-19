991bdfe294af982deadd3d80cf9195d9d2c0f34c
Muhammad Muzammil Umar <muzammil.coredigitals@gmail.com>
2025-03-19 23:16:33 +0500

    track-click-revert

--- track_click.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'db.php';
-
-if (isset($_GET['refurl'])) {
-    $refurl = $conn->real_escape_string($_GET['refurl']);
-    $query = "INSERT INTO click_tracking (refurl, clicked_at) VALUES ('$refurl', NOW())";
-}
-
-?>
