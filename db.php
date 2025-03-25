523bd09aba07b52afcb6519aae515b0f6c5e5e77
Muhammad Muzammil Umar <muzammil.coredigitals@gmail.com>
2025-03-26 03:02:49 +0500

    certifcation-responsive

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 
-// $conn = new mysqli("localhost", "root", "", "safe_speech_cert");
+$conn = new mysqli("localhost", "root", "", "safe_speech_cert");
 
-$conn = new mysqli("localhost", "demoprojectonlin_scc_user", "v5soa@POji4m", "demoprojectonlin_scc");
+// $conn = new mysqli("localhost", "demoprojectonlin_scc_user", "v5soa@POji4m", "demoprojectonlin_scc");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
